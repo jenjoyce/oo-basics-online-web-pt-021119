7ae58fb706db1e668c86769218bd31f81e7c585d
Jennifer Joyce <jenjoyce@mac.com>
2019-03-27 03:54:30 +0000

    Automatically backed up by Learn

--- spec/02_shoe_spec.rb
+++ spec/02_shoe_spec.rb
@@ -1,50 +1,18 @@
-class Book
-  
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
   def initialize(brand)
     @brand = brand
   end
-  
-  def brand 
-    @brand 
-  end
-  
-  def size(size)
-    @size = size
-  end
-  
-  def size
-    @size
-  end
-  
-  def color(color)
-    @color = color
-  end
-  
-  def color
-    @color
-  end
-  
-  def material(material)
-    @material = material
-  end
-  
-  def material
-    @material
-  end
-  
-  def condition(condition)
-    @condition = condition
-  end
-  
-  def condition
-    @condition
-  end
-  
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
   
   def cobbler
     puts "The shoe has been repaired"
   end
-  
 end
 
 
